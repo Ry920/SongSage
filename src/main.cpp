a9c331f06d73738b560e846df45ea19e6822d0e7
rsald <r.saldajeno@ufl.edu>
2024-04-18 16:06:38 -0400

    started working on welcome window: changed window color to be sage, started working on text

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include "songlist.hpp"
 using namespace std;
 
@@ -16,6 +17,7 @@ using namespace std;
 
 int main() {
 	sf::RenderWindow w(sf::VideoMode(800, 600), "test");
+
 	while (w.isOpen()) {
 		for (auto event = sf::Event{}; w.pollEvent(event);)
 		{
@@ -24,7 +26,15 @@ int main() {
 				w.close();
 			}
 		}
+        sf::Text welcomeText;
+        welcomeText.setString("SongSage: Tailored Tunes Just for You");
+        welcomeText.setCharacterSize(48);
+        welcomeText.setFillColor(sf::Color(1, 50, 32));
+
+        w.clear(sf::Color(208, 217, 179));
+
 		w.display();
+
 		w.clear();
 	}
 	return 0;
